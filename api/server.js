699f83aadf2b79172793e08c4eef91805e89e389
CaitlinTalbert <92820628+CaitlinTalbert@users.noreply.github.com>
2022-04-11 15:13:04 -0500

    delete working, MVP

--- api/server.js
+++ api/server.js
@@ -80,4 +80,23 @@ server.put("/api/users/:id", async (req, res) => {
   }
 });
 
+server.delete("/api/users/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const deletedUser = await Users.remove(id);
+    if (!deletedUser) {
+      res.status(404).json({
+        message: "The user with the specified ID does not exist",
+      });
+    } else {
+      res.json(deletedUser);
+    }
+  } catch (err) {
+    res.status(500).json({
+      message: "The user could not be removed",
+      error: err.message,
+    });
+  }
+});
+
 module.exports = server; // EXPORT YOUR SERVER instead of {}
